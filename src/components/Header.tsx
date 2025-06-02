74b258dc5ba40aac0a440de39f4090d53c879c70
Thiago Dias <thiago.dsd@outlook.com>
2025-06-01 23:21:10 -0300

    Refactor Header component for consistency and readability

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,70 +1,71 @@
-"use client";
+'use client'
 
-import Spinner from "./Spinner";
-import Link from "next/link";
-import useSession from "@/lib/useSession";
-import useStore from "@/store/userStore";
-import { apiLogoutUser } from "@/lib/api-requests";
-import { useRouter } from "next/navigation";
+import { apiLogoutUser } from '@/lib/api-requests'
+import useSession from '@/lib/useSession'
+import useStore from '@/store/userStore'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 const Header = () => {
-  const store = useStore();
-  const user = useSession();
-  const router = useRouter();
+  const store = useStore()
+  const user = useSession()
+  const router = useRouter()
 
   const handleLogout = async () => {
-    store.setRequestLoading(true);
+    store.setRequestLoading(true)
     try {
-      await apiLogoutUser();
+      await apiLogoutUser()
     } catch (error) {
     } finally {
-      store.reset();
-      router.push("/login");
+      store.reset()
+      router.push('/login')
     }
-  };
+  }
 
   return (
     <>
-      <header className="bg-white h-20">
-      <nav className="h-full flex flex-row justify-between items-center px-6 w-full">
+      <header className='bg-white h-20'>
+        <nav className='h-full flex flex-row justify-between items-center px-6 w-full'>
           <div>
-            <Link href="/" className="text-ct-primary text-2xl font-semibold">
+            <Link href='/' className='text-ct-primary text-2xl font-semibold'>
               FastFood
             </Link>
           </div>
-          <ul className="flex items-center gap-4">
+          <ul className='flex items-center gap-4'>
             <li>
-              <Link href="/" className="text-ct-dark-600">
+              <Link href='/' className='text-ct-dark-600'>
                 Início
               </Link>
             </li>
+
             {!user && (
               <>
                 <li>
-                  <Link href="/register" className="text-ct-dark-600">
+                  <Link href='/register' className='text-ct-dark-600'>
                     Inscreva-se
                   </Link>
                 </li>
                 <li>
-                  <Link href="/login" className="text-ct-dark-600">
+                  <Link href='/login' className='text-ct-dark-600'>
                     Entrar
                   </Link>
                 </li>
               </>
             )}
+            {/* If user is not authenticated */}
             {user && (
               <>
                 <li>
-                  <Link href="/profile" className="text-ct-dark-600">
+                  <Link href='/profile' className='text-ct-dark-600'>
                     Profile
                   </Link>
                 </li>
                 <li>
-                  <Link href="/chat" className="text-ct-dark-600">
+                  <Link href='/chat' className='text-ct-dark-600'>
                     Chat
                   </Link>
                 </li>
-                <li className="cursor-pointer" onClick={handleLogout}>
+                <li className='cursor-pointer' onClick={handleLogout}>
                   Logout
                 </li>
               </>
@@ -73,7 +74,7 @@ const Header = () => {
         </nav>
       </header>
     </>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
