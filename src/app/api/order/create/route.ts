33b79da52e3bad027eae721a2702c8c17553d3f2
Thiago Dias <thiago.dsd@outlook.com>
2025-06-03 23:43:07 -0300

    Refactor order creation route for improved readability and consistency

--- src/app/api/order/create/route.ts
+++ src/app/api/order/create/route.ts
@@ -1,34 +1,26 @@
-import { getErrorResponse } from "@/lib/helpers";
-import { prisma } from "@/lib/prisma";
-import {
-  RegisterUserInput,
-  RegisterUserSchema,
-} from "@/lib/validations/user.schema";
-import { hash } from "bcryptjs";
-import { cookies } from "next/headers";
-import { NextRequest, NextResponse } from "next/server";
-import { ZodError } from "zod";
+import { getErrorResponse } from '@/lib/helpers'
+import { cookies } from 'next/headers'
+import { NextRequest } from 'next/server'
 
+// This route handles the creation of an order by making a POST request to the core API.
 export async function POST(req: NextRequest) {
-  const cookieStore = cookies();
-  const token = cookieStore.get("session_token")?.value;
-    
-  try {
-    const body = await req.json();
+  const cookieStore = cookies()
+  const token = cookieStore.get('session_token')?.value
 
-    const response = await fetch(`${process.env.NEXT_PUBLIC_CORE_API}/order`, { 
-      method: "POST",
+  try {
+    const body = await req.json()
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_CORE_API}/order`, {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`, 
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(body),
-    });
-  
+    })
 
-    return response;
+    return response
   } catch (error: any) {
-    return getErrorResponse(500, error.message);
+    return getErrorResponse(500, error.message)
   }
 }
-
